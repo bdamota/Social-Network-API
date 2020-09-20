ea4b33285bb89a764a6fa7ff4603eb75c023f312
LYNN DAMOTA <bdamota0506@gmail.com>
2020-09-19 23:05:37 -0700

    thought model created

---
+++ models /Thought.js
@@ -0,0 +1,59 @@
+const { Schema, model, Types } = require('mongoose');
+const moment = require('moment');
+
+const ReplySchema = new Schema(
+    {
+      // set custom id to avoid confusion with parent comment _id
+      reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+      },
+      reactionBody: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1,
+        maxlength: 280
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+      }
+    },
+  );
+
+const ThoughtSchema = new Schema (
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [
+            {
+              type: Schema.Types.ObjectId,
+              ref: 'reactionSchema',
+            }
+          ],
+    }
+)
+
+  // get total count of friends on retrieval
+  ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.reduce((total, reaction) => total + reaction.count.length + 1, 0);
+  });
